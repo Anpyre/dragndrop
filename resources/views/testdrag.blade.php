d4a7b5e4959ef46b0f5d2e5c9528ba498ea71d8d
Audrey <audreyllavador@hotmail.fr>
2019-07-01 20:52:39 +0200

    laravel init + test drag n drop

---
+++ resources/views/testdrag.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="fr">
+   <head>
+        <link rel="stylesheet" href="css/style2.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+       
+   </head>
+        <body>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="dropper1">
+                            <div class="dropper">
+                                <div class="draggable box1">#1</div>
+                                <div class="draggable box2">#2</div>   
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 offset-1">
+                        <div class="dropper2">
+                            <div class="dropper">   
+                                <div class="draggable box3">#3</div>
+                                <div class="draggable box4">#4</div>    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+            <script src="js/drag2.js"></script>
+
+        </body>
+</html>
